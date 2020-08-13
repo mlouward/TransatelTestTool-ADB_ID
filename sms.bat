4e601ac60e06ab2fc1388e776374d418173a2f49
Maxime Louward <maxime.louward@transatel.com>
2020-08-13 16:23:46 +0200

    added sim

--- sms.bat
+++ sms.bat
@@ -7,10 +7,9 @@ adb -s %1 shell "service call isms 7 i32 1 s16 "com.android.mms.service" s16 "%2
 goto End
 
 :Error
-echo Unable to send the SMS. Make sure that the correct phones are connected, and that the text doesn't use any of the forbidden symbols.
+echo Unable to send the SMS. Make sure that the correct phones are connected.
 echo [!date!-!time:~0,8!] Unable to send SMS. (FROM: %3, TO: %2, NB: %4, TEXT: %5)>>..\logs\SMSlog.txt
 
 :End
-echo. >>..\logs\SMSlog.txt
 endlocal
 cd..
